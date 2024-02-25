1f9181cd0e9c36d79b8df8d4b8431b8f8c0ce56e
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-02-25 04:36:33 -0500

    add undo tree

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,4 +1,20 @@
 return {
-	"deris/vim-shot-f",
-	lazy = false,
+  {
+    'deris/vim-shot-f',
+    lazy = false,
+  },
+
+  {
+    'jiaoshijie/undotree',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('undotree').setup()
+      vim.keymap.set('n', '<leader>u', '<cmd>lua require("undotree").toggle()<cr>', { desc = 'Undo Tree' })
+    end,
+    -- keys = { -- load the plugin only when using it's keybinding:
+    --   { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+    -- },
+  },
 }
