mjhika/mjhika.nvim

commit c5ae810117977c35d90f82c9a0ea94bd3cfd0fdf

change listchars
remove tab
remove leadmultispace

i don't like the clutter anymore

lua/opts.lua
@@ -26,4 +26,4 @@ vim.opt.timeout = true -- timeout for which-key
 vim.opt.timeoutlen = 350 -- timeout for which-key
 vim.opt.showmode = false -- disable echoing the mode in favor of the status line
 vim.opt.list = true -- formatting for some whitespace
-vim.opt.listchars = 'tab:┆ ,eol:λ,leadmultispace:┆ ,nbsp:⋅,trail:∘' -- the whitespace replacements
+vim.opt.listchars = 'eol:λ,,nbsp:⋅,trail:∘' -- the whitespace replacements

commit bdc0e7ed8d437ea34a5563eff59c9ed581d5b7a9

fix listchars
fix bug introduced in c5ae8101

lua/opts.lua
@@ -26,4 +26,4 @@ vim.opt.timeout = true -- timeout for which-key
 vim.opt.timeoutlen = 350 -- timeout for which-key
 vim.opt.showmode = false -- disable echoing the mode in favor of the status line
 vim.opt.list = true -- formatting for some whitespace
-vim.opt.listchars = 'eol:λ,,nbsp:⋅,trail:∘' -- the whitespace replacements
+vim.opt.listchars = 'eol:λ,nbsp:⋅,trail:∘' -- the whitespace replacements