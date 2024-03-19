82963dd197f089b540f4043807dee5f09d3de1f2
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-03-19 19:08:05 -0400

    add theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,8 +2,15 @@ return {
   {
     'mcchrish/zenbones.nvim',
     dependencies = 'rktjmp/lush.nvim',
+    -- config = function()
+    --   vim.cmd.colorscheme 'kanagawabones'
+    -- end,
+  },
+  {
+    'nyoom-engineering/oxocarbon.nvim',
+    priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'kanagawabones'
+      vim.cmd.colorscheme 'oxocarbon'
     end,
   },
 }
