01c796b2d0d03d80885bc58cf62cec039d7e8076
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-04-19 00:03:50 -0400

    change the theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,14 +3,21 @@ return {
     'mcchrish/zenbones.nvim',
     dependencies = 'rktjmp/lush.nvim',
     -- config = function()
-    --   vim.cmd.colorscheme 'kanagawabones'
+    --   vim.cmd.colorscheme 'zenbones'
     -- end,
   },
   {
     'nyoom-engineering/oxocarbon.nvim',
     priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme 'oxocarbon'
+    -- end,
+  },
+  {
+    'miikanissi/modus-themes.nvim',
+    priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'oxocarbon'
+      vim.cmd.colorscheme 'modus_operandi'
     end,
   },
 }
