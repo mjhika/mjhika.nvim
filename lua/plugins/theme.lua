9ca7ba410f92d5db2f440a78e0a7aab5ee5dae15
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-04-22 12:19:42 -0400

    update nvim and remove some unused themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,19 +1,5 @@
 return {
   {
-    'mcchrish/zenbones.nvim',
-    dependencies = 'rktjmp/lush.nvim',
-    -- config = function()
-    --   vim.cmd.colorscheme 'zenbones'
-    -- end,
-  },
-  {
-    'nyoom-engineering/oxocarbon.nvim',
-    priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme 'oxocarbon'
-    -- end,
-  },
-  {
     'miikanissi/modus-themes.nvim',
     priority = 1000,
     config = function()
