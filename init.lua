1c33a1a6b62b7dd0093daff47ae7a208aa6751ae
mjhika <1zzt6ovh@mojica.anonaddy.com>
2025-02-26 21:33:10 -0500

    feat: zenburn theme
    change to zenburn

--- init.lua
+++ init.lua
@@ -98,14 +98,11 @@ require('lazy').setup({
   },
 
   {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    'phha/zenburn.nvim',
+    lazy = false,
+    priority = 1000,
     config = function()
-      require('github-theme').setup {}
-
-      vim.cmd.colorscheme 'github_light'
+      require('zenburn').setup()
     end,
   },
 
